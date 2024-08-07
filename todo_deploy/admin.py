9da804c4d05916dd1c5cc4222d140df755f2ee95
alimardon <aboqijonov12@gmail.com>
2024-08-07 17:37:41 +0500

    first commit

---
+++ todo_deploy/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from todo_deploy.models import *
+
+
+# Register your models here.
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'done')
+    list_filter = ('title', 'done')
+    search_fields = ('title',)
